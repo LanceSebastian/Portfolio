68c7fa968a58f8f46a6cc24ab5bad679d87cc961
Lance Vincent Sebastian <76014401+LanceSebastian@users.noreply.github.com>
2025-09-25 00:15:14 +0700

    Update buttons to display modal

--- scripts/courseHandler.js
+++ scripts/courseHandler.js
@@ -1,18 +1,22 @@
+
+var modal = document.getElementById("myModal");
+var content = document.getElementById("content");
+
 fetch("./src/grades.json")
     .then(res => res.json())
     .then(courses => {
-        console.log("Courses loaded:", courses.length);
-        courses.forEach(c => console.log(c.name, c.percent));
         const container = document.getElementById("courseList");
 
         courses.forEach(c => {
             const button = document.createElement("button");
             button.textContent = c.name;
+            button.classList.add(".module");
             
             button.setAttribute("data-id", c.id); // include a reference for future
             
             button.addEventListener("click", (e) => {
-                alert(`You clicked on: ${c.name} (ID: ${e.target.dataset.id})`);
+                content.textContent = `You clicked on: ${c.name} (ID: ${e.target.dataset.id})`;
+                modal.style.display = "block";
             });
 
             container.appendChild(button);
