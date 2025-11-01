a69470eec413f859b709c97c35b052912423d057
Lance Vincent Sebastian <76014401+LanceSebastian@users.noreply.github.com>
2025-11-01 19:48:00 +0800

    update stars to slowly twinkle

--- scripts/frontCover.js
+++ scripts/frontCover.js
@@ -1,10 +1,18 @@
 const canvas = document.getElementById("frontCover");
-const width = canvas.width;
-const height = canvas.height;
+
+const width = canvas.width * 2;
+const height = canvas.height * 2;
+
 const ctx = canvas.getContext("2d");
 const maxStarRadius = 1.5;
+const stars = addStars(width, height, 20);
+const minStarOpacity = 0.1;
+const maxStarOpacity = 0.7;
 
-function addForeground() {    
+var date = new Date();
+let counter = 0;
+
+function addForeground(ctx) {    
     ctx.globalCompositeOperation = "xor";
     ctx.imageSmoothingEnabled = false;
     var image = new Image();
@@ -13,18 +21,31 @@ function addForeground() {
     image.addEventListener("load", (e) => {
         ctx.drawImage(image, 0,0, width, height);
     }); 
+}
 
+function fillCircle(ctx, x, y, r, fillStyle) {
+  ctx.beginPath();
+  ctx.fillStyle = fillStyle;
+  ctx.arc(x, y, r, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+function getOpacity(factor) {
+  const opacityIncrement =
+    (maxStarOpacity - minStarOpacity) * (Math.sin(factor/1000));
+  const opacity = minStarOpacity + opacityIncrement;
+  return opacity;
 }
 
 function addStars(width, height, spacing) {
     const stars = [];
     
-    for(let x = 0; x < width; x += randomInt(spacing)){
-        for(let y = 0; y < height; y += randomInt(spacing)){
+    for(let x = 0; x < width/2; x += spacing){
+        for(let y = 0; y < height/2; y += spacing){
             const star = {
-                x: x,
-                y: y,
-                r: Math.random() * maxStarRadius,
+                x: x + randomInt(spacing),
+                y: y + randomInt(spacing),
+                r: Math.random() * maxStarRadius
             };
             stars.push(star);
         }
@@ -37,21 +58,21 @@ function randomInt(max){
 }
 
 function render(){
-    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fillStyle = "#030318";
     ctx.fillRect(0,0,width,height);
 
-    const stars = addStars(width, height, 30);
 
-    stars.forEach(function(star) {
+    stars.forEach(function(star,i) {
+        const factor = (counter*i);
         const x = star.x;
         const y = star.y;
-        const r = star.r;
-        ctx.beginPath();
-        ctx.fillStyle = "rgb(255, 255, 255)";
-        ctx.arc(x, y, r, 0, Math.PI * 2);
-        ctx.fill();
+        const opacity = getOpacity(factor);
+        fillCircle(ctx, x, y, star.r, `rgba(255, 255, 255, ${opacity}`);
+
     });
+
+    counter++;
+    requestAnimationFrame(render);
 }
 
 render();
-addForeground();
