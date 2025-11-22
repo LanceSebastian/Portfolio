a0a0872e103c5f981f1b3027125d7c23a534954c
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-11-22 18:12:28 +0000

    add Works Slideshow

--- src/scripts/slideshowScript.js
+++ src/scripts/slideshowScript.js
@@ -1,7 +1,8 @@
 
-let slideIndex = [1];
-let slideId = ["slide"]
+let slideIndex = [1,1];
+let slideId = ["photoSlide","workSlide"];
 showSlides(1,0);
+showSlides(1,1);
 
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n,no);
@@ -9,11 +10,11 @@ function plusSlides(n, no) {
 
 function showSlides(n,no) {
     let i;
-    let x = document.getElementsByClassName(slideId[no]);
-    if (n > x.length) {slideIndex[no] = 1}
-    if (n < 1) {slideIndex[no] = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+    let slideshow = document.getElementsByClassName(slideId[no]);
+    if (n > slideshow.length) {slideIndex[no] = 1}
+    if (n < 1) {slideIndex[no] = slideshow.length}
+    for (i = 0; i < slideshow.length; i++) {
+        slideshow[i].style.display = "none";
     }
-    x[slideIndex[no]-1].style.display = "inline";
+    slideshow[slideIndex[no]-1].style.display = "block";
 }
