452ed3b751839f9cd5760293da777ad1d6cbec6d
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-11-27 16:19:48 +0000

    move js code to scripts folder

---
+++ src/scripts/tabScript.js
@@ -0,0 +1,16 @@
+function openContent(evt, contentName){
+    var i, tabContent, tabLinks;
+
+    tabContent = document.getElementsByClassName("tabContent");
+    for(i = 0; i<tabContent.length;i++){
+        tabContent[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for(i = 0; i<tabLinks.length;i++){
+        tabLinks[i].className = tabLinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(contentName).style.display = "grid";
+    evt.currentTarget.className += " active";
+}
