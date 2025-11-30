a29a09aed42839106cae88a4cb8f88fbd68f0d74
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-11-30 19:12:09 +0000

    add shuffle function

--- src/scripts/inspirations.js
+++ src/scripts/inspirations.js
@@ -1,142 +1,142 @@
 const inpsoItems = [
     {
         src: "./src/inspirations/frierenCover.jpg", 
-        title: "My Image", 
+        title: "Frieren: Beyond Journey's End", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/EverythingEverywhereAllAtOnce.jpg",
-        title: "My Image", 
+        title: "Everything Everywhere All at Once", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/LabiSiffre.jpg",
-        title: "My Image", 
+        title: "Bless the Telephone", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/laputa castle in the sky.jpg",
-        title: "My Image", 
+        title: "Laputa: Castle in the Sky", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/marcusAurelius.jpg",
-        title: "My Image", 
+        title: "Meditations", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/minecraftXBOX.jpg",
-        title: "My Image", 
+        title: "Minecraft: Xbox 360 Edition", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/skyrim.jpg",
-        title: "My Image", 
+        title: "Skyrim", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/yourName.jpg",
-        title: "My Image", 
+        title: "Your Name", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/castAway.jpg",
-        title: "My Image", 
+        title: "Cast Away", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/darkSouls3.jpg",
-        title: "My Image", 
+        title: "Dark Souls 3", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/Portal2.jpg",
-        title: "My Image", 
+        title: "Portal 2", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/redDeadRedemption2.jpg",
-        title: "My Image", 
+        title: "Red Dead Redemption 2", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/StardewValley.png",
-        title: "My Image", 
+        title: "Stardew Valley", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/theManFromEarth.jpg",
-        title: "My Image", 
+        title: "The Man From Earth", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/diogenes.png",
-        title: "My Image", 
+        title: "Diogenes", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/intoTheSpiderversejpg.jpg",
-        title: "My Image", 
+        title: "Spider-Man: Into The Spiderverse", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/jackieChan.jpg",
-        title: "My Image", 
+        title: "Jackie Chan", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/pewdiepie.png",
-        title: "My Image", 
+        title: "Pewdiepie", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/sunTzu.jpg",
-        title: "My Image", 
+        title: "Sun Tzu", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/terraria.jpg",
-        title: "My Image", 
+        title: "Terraria", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/dnd.png",
-        title: "My Image", 
+        title: "Dungeons & Dragons", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/doomEternal.jpg",
-        title: "My Image", 
+        title: "Doom: Eternal", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/hollowKnight.jpg",
-        title: "My Image", 
+        title: "Hollow Knight", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/howToTrainYourDragon.jpg",
-        title: "My Image", 
+        title: "How To Train Your Dragon", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/htmmbgd.jpg",
-        title: "My Image", 
+        title: "How to Make Millions before Grandma dies", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/iwteyp.jpg",
-        title: "My Image", 
+        title: "I want to eat your Pancreas", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/spiderMan.png",
-        title: "My Image", 
+        title: "Marvel Spider-Man", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
         src: "./src/inspirations/vanoss.png",
-        title: "My Image", 
+        title: "VanossGaming", 
         desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
 
@@ -145,7 +145,9 @@ const inpsoItems = [
 const inspirationsTemplate = document.getElementById("inspirationsTemplate");
 const inspirationGallery = document.getElementById("inspirations");
 
-inpsoItems.forEach(item => {
+randItems = shuffle(inpsoItems);
+
+randItems.forEach(item => {
     const clone = inspirationsTemplate.content.cloneNode(true);
 
     clone.querySelector("img").src = item.src;
