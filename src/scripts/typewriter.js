9f0aec4f0a80527812fbae229cf4dbb6d8b0de03
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-12-05 10:14:04 +0000

    update responsivity

--- src/scripts/typewriter.js
+++ src/scripts/typewriter.js
@@ -1,17 +1,43 @@
-var i = 0;
+var letter = 0;
+var iSub = 0;
+var randItems = [];
+const subtitleArray = [
+    "I design to your tastes.",
+    "I build to your specifications",
+    "I make it fast for your clients",
+    "I Lorem for your Ipsum",
+    "I maintain for stability"
+];
+
 var txt = "I design and build websites.";
 var maxSpeed = 100;
 
+function getSentence(){
+    var i = iSub % subtitleArray.length;
+    if (i == 0){ randItems = shuffle(subtitleArray);}
+    txt = randItems[i];
+    typeWriter();
+
+}
+
+function backspace(){
+    if (letter <= txt.length){
+        document.getElementById("typewrite").innerHTML = txt.slice(0,-letter);
+        letter++;
+        setTimeout(backspace, maxSpeed * Math.random())
+    } 
+}
+
 function typeWriter(){
-    if (i < txt.length){
-        document.getElementById("typewrite").innerHTML += txt.charAt(i);
-        i++;
-        if (i == txt.length-1){
+    if (letter < txt.length){
+        document.getElementById("typewrite").innerHTML += txt.charAt(letter);
+        letter++;
+        if (letter == txt.length-1){
             setTimeout(typeWriter, 500)
         } else {
             setTimeout(typeWriter, maxSpeed * Math.random())
         }
-    }
+    } 
 }
 
-window.onload = typeWriter;
+window.onload = getSentence;
