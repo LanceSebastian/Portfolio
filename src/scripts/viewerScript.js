a31fb6cc2029296298edfbbf68cdf344163965d5
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-12-09 16:59:07 +0000

    add modal functions

--- src/scripts/viewerScript.js
+++ src/scripts/viewerScript.js
@@ -1,29 +1,34 @@
+let target = document.getElementById("modalTarget");
+
 document.addEventListener("click", e => {
     const item = e.target.closest("figure")
+    const exist = document.querySelector(".modalContent");
     if (!item) return;
-
-    document.querySelectorAll(".viewImage").forEach(e1 => e1.remove())
 
     const imgSrc = item.querySelector("img").src;
     const figCap = item.querySelector("figcaption");
     const title = figCap.querySelector("h3").innerHTML;
     const caption = figCap.querySelector("p").innerHTML;
 
-    const view = document.createElement("div");
-    view.className = "viewImage";
-    view.innerHTML = `
-    <span class="close" onclick="closeView()">&times;</span>
-    <img src="${imgSrc}">
-    <div class = "caption">
-        <h3>${title}</h3>
-        <p>${caption}</p>
+    const modal = document.createElement("div");
+    modal.className = "modalContent";
+    modal.id = "modalContent";
+    modal.innerHTML = `
+    <div class="viewImage">
+        <img src="${imgSrc}">
+        <div class = "caption">
+            <h3>${title}</h3>
+            <p>${caption}</p>
+        </div>
     </div>
-    `
+    `;
 
-    item.insertAdjacentElement("afterend", view);
+    target.style.display = "block";
+    if (exist) exist.replaceWith(modal);
+    else target.append(modal);
 })
 
 
 function closeView() {
-    document.querySelectorAll(".viewImage").forEach(e => e.remove())
+    target.style.display = "none";
 }   
