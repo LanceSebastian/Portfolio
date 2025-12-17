344df1294bec5e764fc0e9bac7e9247821b01ac2
Lance Vincent Sebastian <lvsebastian2002@gmail.com>
2025-12-17 00:34:28 +0000

    add localstorage feature for darkmode

--- src/scripts/darkmode.js
+++ src/scripts/darkmode.js
@@ -1,5 +1,27 @@
-/* Dark Mode Toggle */
+let darkmode = localStorage.getItem("darkmode");
+const darkModeToggle = document.getElementById("darkModeToggle");
+
+const enableDarkMode = () => {
+    document.body.classList.add("dark");
+    localStorage.setItem("darkmode", "active");
+}
+
+const disableDarkMode = () => {
+    document.body.classList.remove("dark");
+    localStorage.setItem("darkmode", "inactive");
+}
+if (darkmode === "active") {
+    enableDarkMode();
+}
+darkModeToggle.addEventListener("click", () => {
+    darkmode = localStorage.getItem("darkmode");
+    darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+});
+
+/* Dark Mode Toggle (old code)*/
+/*
 function toggleDarkMode() {
   var element = document.body;
   element.classList.toggle("dark");
 }
+  */
